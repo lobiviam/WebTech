958d7852170a21b3c6c2fd560ea7dfd45c37632a
olga <lobiviam@gmail.com>
2016-06-23 23:28:13 +0300

    .

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 var express = require('express');
+var sqlite = require('sqlite3');
+
 var app = express();
-app.set('view engine','ejs')
+app.set('view engine','ejs');
 
 //var ejs = require('ejs');
-//var sqlite = require('sqlite3');
 
+var db = new sqlite.Database('db.sqlite');
 
-var app = express();
-app.use(express.static('static'));
+// app.use(express.static('static'));
 
 //app.get('/', function (req, res) {
 //  res.render('WebTask.ejs')
 //});
 
-app.get('/', function (req, res) {
-  res.render('home.ejs',{text: 'my text'});
+app.get('/', function(req, res) {
+  db.all('select * from posts', function(err, data){
+    res.render('home.ejs', {
+      posts: data
+    });
+  });
 });
+
+app.post('/new', function(req, res) {
+  console.log(req.query.text);
+  db.run('insert into posts (text) values(?);', req.query.text, function () {
+    res.send('ok');
+  });
+});
+    
+app.delete('/delete', function (req, res) {
+  db.run('delete from posts (text) where id = ?;', req.query.id, function () {
+    res.send('ok');
+  });  
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
